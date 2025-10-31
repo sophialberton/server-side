a28318c43506e2194b9056f0ea0fbd42e1f57fea
sophialberton <sophia.alberton@gmail.com>
2025-10-31 19:11:22 -0300

    pronto

--- src/routes/associados.routes.js
+++ src/routes/associados.routes.js
@@ -4,13 +4,13 @@ const controleAssociados = require('../controllers/associados.controllers');
 const roteador = express.Router();
 
 // Rotas CRUD
-roteador.post('/associados', controleAssociados.criarAssociado);        // POST: Cria
-roteador.get('/associados', controleAssociados.buscarTodosAssociados);   // GET: Lista todos
+roteador.post('/associados', controleAssociados.criarAssociado); // POST: Cria
+roteador.get('/associados', controleAssociados.buscarTodosAssociados); // GET: Lista todos
 
 // Rotas de Busca, Atualização e Deleção por CPF
-roteador.get('/associados/cpf/:cpf', controleAssociados.buscarPorCpf);    // GET: Busca por CPF (adicional)
-roteador.get('/associados/:cpf', controleAssociados.buscarPorCpf);        // GET: Busca por CPF
-roteador.put('/associados/:cpf', controleAssociados.atualizarAssociado);  // PUT: Atualiza por CPF
+roteador.get('/associados/cpf/:cpf', controleAssociados.buscarPorCpf);// GET: Busca por CPF (adicional)
+roteador.get('/associados/:cpf', controleAssociados.buscarPorCpf); // GET: Busca por CPF
+roteador.put('/associados/:cpf', controleAssociados.atualizarAssociado); // PUT: Atualiza por CPF
 roteador.delete('/associados/:cpf', controleAssociados.deletarAssociado); // DELETE: Deleta por CPF
 
 module.exports = roteador;
